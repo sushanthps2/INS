86ddeac9be93d6ba473d214161e1ad3b9ca4e9f6
sushanthps2 <35593841+sushanthps2@users.noreply.github.com>
2019-02-25 11:15:23 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<string.h>
+#define N strlen(g)
+void crc();
+void xor();
+char g[20];
+char text[100];
+char code[100];
+int len;
+int main()
+{
+	int i,flag=0;
+	printf("Enter the message in binary form\n");
+	scanf("%s",text);
+	printf("Enter generation number\n");
+	scanf("%s",g);
+	len=strlen(text);
+	for(i=len;i<len+N-1;i++)
+		text[i]='0';
+	crc();
+	for(i=len;i<len+N-1;i++)
+		text[i]=code[i-len];
+	printf("Final codeword: %s\n",text);
+	printf("Enter the coded word: ");
+	scanf("%s",text);
+	crc();
+	for(i=0;i<strlen(code);i++)
+		if(code[i]=='1')
+		{
+			flag=1;
+			break;
+		}
+	if(flag==1)
+		printf("ERROR\n");
+	else
+		printf("NO ERROR\n");
+	return 0;
+}
+void crc()
+{
+	int i,j;
+	for(i=0;i<N;i++)
+		code[i]=text[i];
+	do
+	{
+		if(code[0]=='1')
+			xor();
+		for(j=0;j<N-1;j++)
+			code[j]=code[j+1];
+		code[j]=text[i];
+		i++;
+	}while(i<=len+N-1);
+}
+void xor()
+{
+	int i;
+	for(i=0;i<N;i++)
+		if(code[i]==g[i])
+			code[i]='0';
+		else
+			code[i]='1';
+}
---
+++ program3.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int mult(unsigned int x, unsigned int y, unsigned int n)
+{
+	unsigned long int k=1;
+	int j;
+	for(j=1;j<=y;j++) k=(k*x)%n;
+	return (unsigned int) k;
+}
+void main()
+{
+	char msg[100];
+	unsigned int pt[100], ct[100], n, d, e, p, q, i;
+	printf("Enter message:");
+	scanf("%[^\n]s",msg);
+	printf("%s",msg);
+	for(i=0;i<strlen(msg);i++)
+		pt[i]=msg[i];
+	n=253; d=17; e=13;
+	printf("\nCT=");
+	for(i=0;i<strlen(msg);i++) ct[i]=mult(pt[i],e,n);
+	for(i=0;i<strlen(msg);i++) printf("%d",ct[i]);
+	printf("\nPT=");
+	for(i=0;i<strlen(msg);i++) printf("%c", pt[i]);
+	for(i=0;i<strlen(msg);i++) pt[i]=mult(ct[i],d,n);
+	printf("\n");
+}
